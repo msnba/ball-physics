f08e4087dd7366659747922da8ccfbea3c97afa0
msnba <alex.n.coviello@gmail.com>
2025-08-17 10:54:10 -0500

    created simple window and added GLAD as a package

--- main.cpp
+++ main.cpp
@@ -1,5 +1,54 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from BallPhysics!\n";
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                 // version 3.3
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // using core means we'll only use/get features we actually need
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Window", NULL, NULL);
+
+    if (!window)
+    {
+        std::cout << "Failed window creation!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed GLAD initialization!" << std::endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // registering function to call every window resize
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window); // swap frame buffers every refresh, using double buffering
+        glfwPollEvents();        // sync events
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
